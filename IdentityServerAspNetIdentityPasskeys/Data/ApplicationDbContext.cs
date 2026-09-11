bab00a7d32b040fc76289ba160736b9bf66c185c
ArcherTrister <archertrister@outlook.com>
2026-09-11 13:08:54 +0800

    chore: sync local changes

--- IdentityServerAspNetIdentityPasskeys/Data/ApplicationDbContext.cs
+++ IdentityServerAspNetIdentityPasskeys/Data/ApplicationDbContext.cs
@@ -34,13 +34,35 @@ public class ApplicationDbContext : IdentityDbContext<
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
+    
+        // builder.Entity<IdentityUserPasskey<string>>(b =>
+        // {
+        //     b.HasKey(p => p.CredentialId);
+        //     b.ToTable("AspNetUserPasskeys");
+        //     b.Property(p => p.CredentialId).HasMaxLength(1024); // Defined in WebAuthn spec to be no longer than 1023 bytes
+        //     b.OwnsOne(p => p.Data).ToJson();
+        // });
+        // builder.Entity<IdentityUserPasskey<string>>(b =>
+        // {
+        //     b.HasKey(p => p.CredentialId);
+        //     b.ToTable("AspNetUserPasskeys");
+        //     b.Property(p => p.CredentialId).HasMaxLength(1024);
+        //     // Unable to create a 'DbContext' of type 'ApplicationDbContext'.
+        //     // The exception 'The property or navigation 'Data' cannot be added to the 'IdentityUserPasskey<string>' type because a property or navigation with the same name already exists on the 'IdentityUserPasskey<string>' type.' was thrown while attempting to create an instance.
+        //     // For the different patterns supported at design time, see https://go.microsoft.com/fwlink/?linkid=851728   
+        //     b.ComplexProperty(p => p.Data).ToJson();
+        // });
         
-        builder.Entity<IdentityUserPasskey<string>>(b =>
-        {
-            b.HasKey(p => p.CredentialId);
-            b.ToTable("AspNetUserPasskeys");
-            b.Property(p => p.CredentialId).HasMaxLength(1024);
-            b.OwnsOne(p => p.Data).ToJson();
-        });
+        // // 配置 IdentityUserPasskey
+        // var passkey = builder.Entity<IdentityUserPasskey<string>>();
+        //
+        // // 先移除已存在的 Data 属性（如果存在）
+        // if (passkey.Metadata.FindProperty(nameof(IdentityUserPasskey<string>.Data)) != null)
+        // {
+        //     passkey.Ignore(nameof(IdentityUserPasskey<string>.Data));
+        // }
+        //
+        // // 然后再添加为 ComplexProperty
+        // passkey.ComplexProperty(p => p.Data).ToJson();
     }
 }
